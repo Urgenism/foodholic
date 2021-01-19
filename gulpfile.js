b05f9c04b17f0c9da2816e6a2ed6abd90b9ed45d
Urgen Sherpa <urgen444@gmail.com>
2021-01-19 21:13:11 +0545

    gulp watch command updated to take build first

--- gulpfile.js
+++ gulpfile.js
@@ -11,19 +11,29 @@ const js = require("./gulp/scripts");
 
 // Watch files
 function watchFiles() {
-    gulp.watch("./src/assets/scss/**/*", css.buildCss);
-    gulp.watch("./src/assets/js/**/*", scripts);
-    gulp.watch("./src/assets/img/**/*", images.optimizeImages);
-    gulp.watch("./src/*.html", copy.copyHtml);
-    gulp.watch("./src/assets/fonts/**/*", copy.copyFonts);
+  gulp.watch("./src/assets/scss/**/*", css.buildCss);
+  gulp.watch("./src/assets/js/**/*", scripts);
+  gulp.watch("./src/assets/img/**/*", images.optimizeImages);
+  gulp.watch("./src/*.html", copy.copyHtml);
+  gulp.watch("./src/assets/fonts/**/*", copy.copyFonts);
 }
 
 // define complex tasks
 const scripts = gulp.series(js.lint, js.build);
-const watch = gulp.parallel(watchFiles, server.init, server.reload);
 const build = gulp.series(
-    clean.all,
-    gulp.parallel(copy.copyHtml, copy.copyFonts, css.buildCss, css.minifyCSS, images.optimizeImages, scripts)
+  clean.all,
+  gulp.parallel(
+    copy.copyHtml,
+    copy.copyFonts,
+    css.buildCss,
+    css.minifyCSS,
+    images.optimizeImages,
+    scripts
+  )
+);
+const watch = gulp.series(
+  build,
+  gulp.parallel(watchFiles, server.init, server.reload)
 );
 
 // expose tasks to CLI
